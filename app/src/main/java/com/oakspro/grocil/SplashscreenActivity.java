1c5c3318a66e5da3de32fe5a1d7933a3be9996aa
ReddyGona <gonajanardhan@karunya.edu.in>
2021-02-04 20:24:57 +0530

    added SplashscreenActivity

---
+++ app/src/main/java/com/oakspro/grocil/SplashscreenActivity.java
@@ -0,0 +1,27 @@
+package com.oakspro.grocil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent i = new Intent(SplashscreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 800);
+
+    }
+}
